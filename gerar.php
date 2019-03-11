4dede60c3a84136dee97ed8e09d2b9bdab6b25ed
Vitor Matos <vitormatos@backsite.com.br>
2019-03-11 10:43:58 -0300

    Versão 2 do gerador

--- gerar.php
+++ gerar.php
@@ -1,19 +1,20 @@
 <?php
 
-function GerarCodigoGlovo() {
+function GerarCodigoGlovo($entrada) {
     //Caminho para o arquivo de palavras
     $caminho = "/base/palavras.txt/";
     //Lê o arquivo, se não for possível retorna o erro
-    if (!$entrada = file_get_contents($caminho)) {
-        return "O arquivo não foi encontrado no caminho indicado.";
-    }
+	if (!$entrada = file_get_contents($caminho)) {
+	   return "O arquivo não foi encontrado no caminho indicado.";
+	}
     //Retorna um array de uma string de palavras separadas por ","
-    function explode_palavras($palavras) {
-        $palavras = str_replace("\n", "",$entrada);
-        $palavras = explode(",",$entrada);
+    function explode_palavras($string) {
+        $palavras = explode(",",$string);
         return $palavras;
     }
-    //cria uma cópia da var entrada
+	$entrada = str_replace("<br>", "",str_replace("	", "",str_replace("\r", "",str_replace(" ", "",str_replace("\n", "",$entrada)))));
+	$palavras = explode_palavras($entrada);
+	    //cria uma cópia da var entrada
     $palavras_temporarias = $entrada;
     //Define valor default para as variaveis
     $saida = array();
@@ -26,32 +27,34 @@ function GerarCodigoGlovo() {
     $qt_codigos = ($qt_palavras*($qt_palavras-1)*($qt_palavras-2))/100;
     //Gera essa quantidade de códigos
     while($i < $qt_codigos) {
-            //valores defaults para as var
-            $j=0;
-            $codigo = "";
+        //valores defaults para as var
+        $j=0;
+        $codigo = "";
+        $qt_palavras = count($palavras);
 
-            //Gera um numero randomico entre 2 e 3 para codigos de 2 ou de 3 palavras
-            while($j<rand(2,3)) {
-                //Escolhe randomicamente uma palavra no array;
-                $palavra = $palavras[rand(0,$qt_palavras-1)];
-                //Retira a palavra da string de cópia;
-                $palavras_temporarias = str_replace(",,",",",str_replace($palavra,"",$entrada));
-                //Explode novamente as palavras, agora sem a palavra anterior;
-                $palavras = explode_palavras($palavras_temporarias);
-                //concatena em codigo
-                $codigo .= $palavra;
-                //decrementa a quantidade de palavras
-                $qt_palavras--;
-                //incrementa o contador
-                $j++;
-            }
-            //Se o codigo se gerado não se repetir, guarda!
-            if (!in_array($codigo, $saida)) {
-                $saida[] = $codigo;
-            }
-            $i++;
+        //Gera um numero randomico entre 2 e 3 para codigos de 2 ou de 3 palavras
+        while($j<rand(2,3)) {
+            //Escolhe randomicamente uma palavra no array;
+            $rand = rand(0,$qt_palavras-1);
+            $palavra = $palavras[$rand];
+            //Retira a palavra da string de cópia;
+            $palavras_temporarias = str_replace(",,",",",str_replace($palavra,"",$entrada));
+            //Explode novamente as palavras, agora sem a palavra anterior;
+            $palavras = explode_palavras($palavras_temporarias);
+            //concatena em codigo
+            $codigo .= $palavra;
+            //decrementa a quantidade de palavras
+            $qt_palavras--;
+            //incrementa o contador
+            $j++;
         }
+        //Se o codigo se gerado não se repetir, guarda!
+        if (!in_array($codigo, $saida)) {
+            $saida[] = $codigo;
+        }
+        $i++;
+    }
     return $saida;
 }
 
-echo_r(GerarCodigoGlovo());
+print_r(GerarCodigoGlovo($arquivo));
